b1dc23b20b03f7811e16bae24d737ab84f0e696d
pjamsa <Paul_Jamsa@msn.com>
2021-03-25 13:01:30 -0700

    Create main.js

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,27 @@
+// My first JavaScript code
+
+window.alert("Greeting, World");
+document.write("Hello, World");
+
+var Reptile = "A snake is a reptile"; //assigning a variable a string and printing it
+document.write(Reptile);
+
+window.alert(Reptile);
+
+document.write("There\'s no way your getting away with that!");
+
+
+document.write("\"Be who you are," + " and be who you be," + "\ I am not you, and you are not me.\""); //Concatenating a string
+
+var Mammal = "Monkey", Reptile = "Snake";
+document.write(Reptile + Mammal);
+
+
+
+var Reptile = Reptile.fontcolor("green"); // Changing fonts of Reptile and Mammal Variables
+var Mammal = Mammal.fontcolor("brown");
+document.write(Reptile);
+document.write(Mammal);
+
+
+document.write(3 * 1000); // writing an JS expression and displaying it in the browser
